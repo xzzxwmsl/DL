2511ab4634937840bb8f664aeae89e9fe0cece4e
xzzxwmsl <1365303748@qq.com>
2020-09-26 13:05:56 +0800

    自己写了一编线性回归的作业

---
+++ AndrewNgCourse/Code/LinearRegression/homework_1.py
@@ -0,0 +1,6 @@
+import LinearRegression as LR
+
+
+def __main__():
+    a = 1
+    print()
--- AndrewNgCourse/Code/pythonReview/numpyTest.py
+++ AndrewNgCourse/Code/pythonReview/numpyTest.py
@@ -1,5 +1,32 @@
 import numpy as np
 import pandas as pd
+import os
+import numpy as np
 
-data = pd.read_csv('ex1data1.txt', header=None, names=['A', 'B'])
-data.head()
+# dataPath = 'C:\\Users\\13653\\Desktop\\Code\\ML\\AndrewNgCourse\\Code\\pythonReview\\data.txt'
+# 使用绝对路径读取文件
+dataPath = os.path.dirname(os.path.abspath(__file__)) + '\\data.txt'
+data = pd.read_csv(dataPath, header=None, names=['Population', 'Profit'])
+data.insert(0, 'One', 1)
+print(data.head())
+
+X = data.iloc[:, 0:2]
+y = data.iloc[:, 2:3]
+# X = np.array(X)  # 97 * 2
+# y = np.array(y)  # 97 * 1
+X = np.matrix(X.values)
+y = np.matrix(y.values)
+theta = np.matrix([0, 0])
+print(np.sum(np.power(y, 2)))
+# #
+# x = np.array([[1, 2], [1, 3], [1, 4]])
+# print(np.sum(x))
+# y = np.array([1, 1, 1])
+# theta = np.array([1, 1])
+# print(x @ theta.T)
+#
+# re = (x @ theta.T) - y
+# print('re:', re)
+# re = np.power(re, 2)
+# print(re)
+# print(np.sum(re))
